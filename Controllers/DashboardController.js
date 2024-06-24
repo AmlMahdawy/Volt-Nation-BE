084c5005c4fec209d70f417794254af22745b3f3
Aml Ibrahim (Mully) <amlmahdawy21@gmail.com>
2024-06-24 03:10:04 +0300

    editing charts

--- Controllers/DashboardController.js
+++ Controllers/DashboardController.js
@@ -9,6 +9,22 @@ const GetStatistics = async (req, res) => {
     let users = await UserModel.find({}, { _id: 1 })
     let orders = await OrderModel.find({}, { date: 1, totalPrice: 1 })
 
+
+    const monthMap = {
+        1: "Jan",
+        2: "Feb",
+        3: "Mar",
+        4: "Apr",
+        5: "May",
+        6: "Jun",
+        7: "Jul",
+        8: "Aug",
+        9: "Sep",
+        10: "Oct",
+        11: "Nov",
+        12: "Dec"
+    };
+
     const result = await OrderModel.aggregate([
         {
             $group: {
@@ -22,22 +38,15 @@ const GetStatistics = async (req, res) => {
         {
             $project: {
                 _id: 0,
-                month: {
-                    $concat: [
-                        { $toString: "$_id.year" },
-                        "-",
-                        { $cond: { if: { $lt: ["$_id.month", 10] }, then: { $concat: ["0", { $toString: "$_id.month" }] }, else: { $toString: "$_id.month" } } }
-                    ]
-                },
+                year: "$_id.year",
+                month: { $arrayElemAt: [Object.values(monthMap), { $subtract: ["$_id.month", 1] }] },
                 totalMonthlyPrice: 1
             }
         },
         {
-            $sort: { month: 1 } // Sort by month in ascending order
+            $sort: { year: 1, month: 1 } // Sort by year and month in ascending order
         }
     ]);
-
-
 
     let sales = 0;
     totalSales.forEach((obj) => {
